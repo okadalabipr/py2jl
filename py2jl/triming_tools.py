93aa1ba9da2b5787a19b4a048f3c0b845f90d9aa
himoto <himoto@protein.osaka-u.ac.jp>
2020-02-14 11:29:38 +0900

    Update trimming_tools

--- py2jl/triming_tools.py
+++ py2jl/triming_tools.py
@@ -1,7 +1,7 @@
 def lines_triming(lines, space_num=4):
 
     lines = remove_backslash(lines)
-    lines = normalize_blanks(lines,space_num)
+    lines = normalize_blanks(lines, space_num)
     lines = replace_characters(lines)
     lines = convert_comment_out(lines)
 
@@ -9,37 +9,39 @@ def lines_triming(lines, space_num=4):
 
 
 def remove_backslash(lines):
-    for i,line in enumerate(lines):
-        if line.find('param_names = [\\')!=-1 or line.find('var_names = [\\')!=-1 \
-            or line.find('\'len_f_params\'\\') != -1 or line.find('\'len_f_vars\'\\') != -1:
-            lines[i] = lines[i].replace('\\',' ')
-    
+    for i, line in enumerate(lines):
+        if (line.find('param_names = [\\') != -1
+                or line.find('var_names = [\\') != -1
+                or line.find('\'len_f_params\'\\') != -1
+                or line.find('\'len_f_vars\'\\') != -1):
+            lines[i] = lines[i].replace('\\', ' ')
         else:
             while(lines[i][-3:].find('\\\n') != -1):
-                lines[i] = lines[i].rstrip().replace('\\',' ') + lines[i+1].lstrip()
+                lines[i] = lines[i].rstrip().replace('\\', ' ') + lines[i+1].lstrip()
                 lines.pop(i+1)
-    
+
     return lines
 
 
-def normalize_blanks(lines,space_num):
+def normalize_blanks(lines, space_num):
     for i, line in enumerate(lines):
         lines[i] = line.replace('\t', '    ')
         if line.strip(' ') == '':
             lines[i] = '\n'
 
     for i, line in enumerate(lines):
-        lines[i] = normalize_indent(line,space_num)
+        lines[i] = normalize_indent(line, space_num)
 
     return lines
 
 
-def normalize_indent(line,space_num):
-    indent='    '
-    indents=''
+def normalize_indent(line, space_num):
+    indent = '    '
+    indents = ''
     for i in range(space_counter(line)//space_num):
         indents += indent
     line = indents + line.lstrip(' ')
+    
     return line
 
 
@@ -50,17 +52,18 @@ def space_counter(line):
             count += 1
         else:
             break
+        
     return count
 
 
 def replace_characters(lines):
-    for i,line in enumerate(lines):
+    for i, line in enumerate(lines):
         rep_line = line
         if (rep_line.find('if ') != -1
-            or rep_line.find('elif ') != -1
-            or rep_line.find('else ') != -1
-            or rep_line.find('for ') != -1) \
-            and rep_line.find(':') != -1:
+                or rep_line.find('elif ') != -1
+                or rep_line.find('else ') != -1
+                or rep_line.find('for ') != -1) \
+                and rep_line.find(':') != -1:
             rep_line = rep_line.replace(':', '')
 
         rep_line = rep_line.replace('x[C.', 'p[C.')
@@ -97,18 +100,18 @@ def search_end(lines):
     bracket1 = 0
     bracket2 = 0
     bracket3 = 0
-    
+
     for i, line in enumerate(lines):
         if line.strip() == '':
             ind = prev
             #print(i,' is blank line')
-        elif bracket1> 0 or bracket2> 0 or bracket3> 0:
+        elif bracket1 > 0 or bracket2 > 0 or bracket3 > 0:
             ind = prev
         else:
             c = 0
             ind = 0
-            while(line[c]==' '):
-                c+=1
+            while(line[c] == ' '):
+                c += 1
             ind = line[:c].count('    ')
 
             if line.find('else') != -1 or line.find('elif') != -1:
@@ -118,15 +121,15 @@ def search_end(lines):
         if line.count('(') - line.count(')') > 0:
             bracket1 += line.count('(') - line.count(')')
         else:
-            bracket1 -=  line.count(')') - line.count('(')
+            bracket1 -= line.count(')') - line.count('(')
         if line.count('{') - line.count('}') > 0:
             bracket2 += line.count('{') - line.count('}')
         else:
-            bracket2 -=  line.count('}') - line.count('{')
+            bracket2 -= line.count('}') - line.count('{')
         if line.count('[') - line.count(']') > 0:
             bracket3 += line.count('[') - line.count(']')
         else:
-            bracket3 -=  line.count(']') - line.count('[')
+            bracket3 -= line.count(']') - line.count('[')
         indents.append(ind)
 
     for i in range(len(lines)):
@@ -143,18 +146,18 @@ def search_end(lines):
         # print(indents[i],line.rstrip())
     # print(end_lines)
 
-    #for i,line in enumerate(lines):
+    # for i,line in enumerate(lines):
     #    print(indents[i],line.replace('\n',''))
     return end_lines
 
 
 def convert_comment_out(lines):
-    count=0
-    for i,line in enumerate(lines):
+    count = 0
+    for i, line in enumerate(lines):
         if line.find('\'\'\'') != -1:
             count += 1
-        if count%2 == 1:
-            lines[i] = line.replace('\'\'\'','#=')
+        if count % 2 == 1:
+            lines[i] = line.replace('\'\'\'', '#=')
         else:
-            lines[i] = line.replace('\'\'\'','=#')
+            lines[i] = line.replace('\'\'\'', '=#')
     return lines
