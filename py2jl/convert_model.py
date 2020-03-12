94658680ab2b12e481632c565f0d4927d2ec4989
himoto <himoto@protein.osaka-u.ac.jp>
2020-03-12 13:07:01 +0900

    Update convert_model

--- py2jl/convert_model.py
+++ py2jl/convert_model.py
@@ -12,10 +12,6 @@ def convert_model(jl_dir, py_dir):
         )
     )
     '''
-
-    os.makedirs(
-        jl_dir + '/model', exist_ok=True
-    )
     os.makedirs(
         jl_dir + '/model/name2idx', exist_ok=True
     )
@@ -48,10 +44,11 @@ def make_parameters(jl_dir, py_dir, space_num=4):
     with open(py_dir, mode='r') as f:
         lines = f.readlines()
     lines = triming_tools.lines_triming(lines, space_num)
-    lines = triming_tools.cut_out_lines(lines, 'param_names','len_f_params')[1:]
-
+    lines = triming_tools.cut_out_lines(
+        lines, 'param_names', 'len_f_params'
+    )[1:]
     parameters = jl_source.parameters_header()
-    for i,line in enumerate(lines):
+    for i, line in enumerate(lines):
         parameters += line
     parameters = parameters + jl_source.parameters_footer()
 
@@ -66,10 +63,11 @@ def make_variables(jl_dir, py_dir, space_num=4):
     with open(py_dir, mode='r') as f:
         lines = f.readlines()
     lines = triming_tools.lines_triming(lines, space_num)
-    lines = triming_tools.cut_out_lines(lines, 'var_names','len_f_vars')[1:]
-
+    lines = triming_tools.cut_out_lines(
+        lines, 'var_names', 'len_f_vars'
+    )[1:]
     variables = jl_source.variables_header()
-    for i,line in enumerate(lines):
+    for i, line in enumerate(lines):
         variables += line
     variables = variables + jl_source.variables_footer()
 
@@ -84,10 +82,11 @@ def make_param_const(jl_dir, py_dir, space_num=4):
     with open(py_dir, mode='r') as f:
         lines = f.readlines()
     lines = triming_tools.lines_triming(lines, space_num)
-    lines = triming_tools.cut_out_lines(lines, 'len_f_params','return x')[1:]
-
+    lines = triming_tools.cut_out_lines(
+        lines, 'len_f_params', 'return x'
+    )[1:]
     param_const = jl_source.param_const_header()
-    for i,line in enumerate(lines):
+    for i, line in enumerate(lines):
         param_const += line
     param_const += jl_source.param_const_footer()
     with open(jl_dir, mode='w') as f:
@@ -101,10 +100,11 @@ def make_initial_condition(jl_dir, py_dir, space_num=4):
     with open(py_dir, mode='r') as f:
         lines = f.readlines()
     lines = triming_tools.lines_triming(lines, space_num)
-    lines = triming_tools.cut_out_lines(lines, 'len_f_vars','return y0')[1:]
-
+    lines = triming_tools.cut_out_lines(
+        lines, 'len_f_vars', 'return y0'
+    )[1:]
     initial_condition = jl_source.initial_condition_header()
-    for i,line in enumerate(lines):
+    for i, line in enumerate(lines):
         initial_condition += line
     initial_condition += jl_source.initial_condition_footer()
 
@@ -122,23 +122,28 @@ def make_differential_equation(jl_dir, py_dir, space_num=4):
     lines = triming_tools.insert_end(lines)
     differential_equation = jl_source.differential_equation_header1()
 
-    for i,line in enumerate(lines):
-        rep_line =line.replace(' ','')
-        if rep_line.find('[0]*')!=-1:
-            if rep_line.find('dydt')==-1:
-                rep_line_trim = rep_line.replace(' ','').strip()
-                vector_name = rep_line_trim[:rep_line_trim.find('=')]
-                elemets_num = rep_line_trim[rep_line_trim.find('[0]*')+4:rep_line_trim.find('#')]
-                lines[i] = jl_source.differential_equation_header2(vector_name,elemets_num)
-                if rep_line_trim.find('#')!=-1:
-                    lines[i] = lines[i].rstrip() + ' ' + rep_line[rep_line.find('#'):]
+    for i, line in enumerate(lines):
+        rep_line = line.replace(' ', '')
+        if rep_line.find('[0]*') != -1:
+            if rep_line.find('dydt') == -1:
+                rep_line_trim = rep_line.replace(' ', '').strip()
+                vector_name = \
+                    rep_line_trim[:rep_line_trim.find('=')]
+                elements_num = \
+                    rep_line_trim[rep_line_trim.find('[0]*')+4:rep_line_trim.find('#')]
+                lines[i] = jl_source.differential_equation_header2(
+                    vector_name, elements_num
+                )
+                if rep_line_trim.find('#') != -1:
+                    lines[i] = lines[i].rstrip() + ' ' + \
+                        rep_line[rep_line.find('#'):]
             else:
                 lines[i] = ''
 
     is_keyword = False
     for i, line in enumerate(lines):
         if (lines[i-1].replace(' ', '').find('diffeq(t,y,x):') != -1
-            or lines[i-1].replace(' ', '').find('diffeq(y,t,*x):') != -1):
+                or lines[i-1].replace(' ', '').find('diffeq(y,t,*x):') != -1):
             is_keyword = True
             #print('\n\n\ntest:start of values is found')
         elif is_keyword and line.find('return dydt') != -1:
